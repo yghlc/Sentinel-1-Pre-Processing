11e062f5e899fd166d730a85085c92fe5b2460d0
Lingcao Huang <huanglingcao@gmail.com>
2022-11-02 12:30:15 -0600

    flood detection, change Float to Byte, add compression

--- flood_detection/houston_HurricaneHarvey_2017.sh
+++ flood_detection/houston_HurricaneHarvey_2017.sh
@@ -17,7 +17,16 @@ ${down_py} ${ext_shp} -d ${save_dir}  -s ${s_date} -e ${e_date} -u ${username} -
 # Apply Orbit File, Remove GRD Border Noise, Calibration, Speckle Filter, and Terrain Correction
 rtc_py=~/codes/PycharmProjects/yghlc_Sentinel-1-Pre-Processing/snap_GRD_process.py
 outdir=${save_dir}/pre-processed
-${rtc_py} one_zip.txt -d ${outdir}
+${rtc_py} all_zip.txt -d ${outdir}
+
+
+# run flood detection
+#fd_py=~/codes/PycharmProjects/yghlc_Sentinel-1-Flood-Detection/SAR_Flood_Detection_v02.py
+#python ${fd_py} fd_houston_2017_inputs.txt
+fd_py=~/codes/PycharmProjects/yghlc_Sentinel-1-Flood-Detection/sar_flood_det.py
+water_mask=~/Data/global_surface_water/extent_epsg2163_houston/extent_100W_30_40N_v1_3_2020.tif
+fd_dir=${save_dir}/FD_results_v2
+${fd_py} all_VV_Sigma0_images.txt -w ${water_mask} -d ${fd_dir}
 
 
 
