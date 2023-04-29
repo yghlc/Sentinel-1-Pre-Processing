6adffe11e8a3b2b9fa5d4785402bdf188403827f
Lingcao Huang <huanglingcao@gmail.com>
2023-04-29 09:01:47 -0700

    check existence of the json file

--- genTools.py
+++ genTools.py
@@ -9,6 +9,8 @@ import os
 import json
 
 def read_dict_from_txt_json(file_path):
+    if os.path.isfile(file_path) is False:
+        raise IOError('%s does not exist'%os.path.abspath(file_path))
     if os.path.getsize(file_path) == 0:
         return None
     with open(file_path) as f_obj:
